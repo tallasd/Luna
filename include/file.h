a203c92381634532b1c94926e2b321c952ce1da3
Ixaruz <44379110+Ixaruz@users.noreply.github.com>
2020-12-25 22:40:50 +0100

    big boy buffer

--- include/file.h
+++ include/file.h
@@ -15,8 +15,8 @@
 #include "log.hpp"
 #include "fsfile.h"
 
-//32kb of buffer bc tesla got syssy memory pool //WerWolv said he uses 4kb
-#define BUFF_SIZE 0x8000
+//128kb of buffer bc tesla got syssy memory pool //WerWolv said he uses 4kb
+#define BUFF_SIZE 0x20000
 
 namespace fs
 {
