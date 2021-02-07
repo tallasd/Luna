fa2fbc7e4d905c64bf5a033ea57f35ca0c5ff4d2
Ixaruz <44379110+Ixaruz@users.noreply.github.com>
2021-02-07 21:07:45 +0100

    Update main.cpp
    For libnx 4.0.0

--- source/main.cpp
+++ source/main.cpp
@@ -73,8 +73,8 @@ public:
     }
 
     // Called once every frame to handle inputs not handled by other UI elements
-    virtual bool handleInput(u64 keysDown, u64 keysHeld, touchPosition touchInput, JoystickPosition leftJoyStick, JoystickPosition rightJoyStick) override {
-        return false;   // Return true here to singal the inputs have been consumed
+    virtual bool handleInput(u64 keysDown, u64 keysHeld, const HidTouchState &touchPos, HidAnalogStickState joyStickPosLeft, HidAnalogStickState joyStickPosRight) override {
+        return false;   // Return true here to signal the inputs have been consumed
     }
 
 private:
